513f8dfee2831025911c21f5b8d51332a92c9bac
Your Name <etholleman@ucdavis.edu>
2021-05-12 14:27:23 -0700

    update structure score calculation script and remove fastq file

--- scripts/calculate_struct_scores.py
+++ scripts/calculate_struct_scores.py
@@ -11,3 +11,24 @@ def struct_score(DSi, SSi):
     '''
     return np.log2(DSi + np.sqrt(1 + np.pow(DSi, 2))) - np.log2(SSi + np.sqrt(1 + np.pow(SSi, 2)))
 
+
+def struct_score_overlap(line):
+    # column 3 and 7 should be scores
+    # we are going to assume first is DSi and second is SSi
+    return struct_score(line[3], line[7])
+
+
+def struct_score_single(line, DSi=0, SSi=0):
+    # calculate struct score for a region that has no overlapping
+    # complement RNA-seq
+    if DSi:
+        DSi = DSi[3]
+    elif SSi:
+        SSi = SSi[3]
+    else:
+        raise Exception('Either DSi or SSi must be a list!')
+    return struct_score(DSi=DSi, SSi=SSi)
+
+
+
+
